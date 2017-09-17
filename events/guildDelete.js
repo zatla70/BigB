e12b55110ebba2c231a983161802646b5baeb311
TheLearneer <bsantosh909@gmail.com>
2017-09-17 20:17:22 +0545

    Error fix and update Fixed a little error upon joining and leaving the server, and update the prefix for IPT.

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,7 +4,7 @@ module.exports = class {
   }
 
   async execute(guild) {
-    await this.client.guilds.get("358098380729221133").channels.get('358098762263953408').send(`I just left the server **${guild.name}**`).catch(e => console.error(e));
+    await this.client.guilds.get("358098380729221133").channels.get('358928336401203200').send(`I just left the server **${guild.name}**`).catch(e => console.error(e));
     await this.client.user.setGame(`${this.client.config.setting.prefix}help | ${this.client.guilds.size} Servers`);
     serverData[guild.id] = {
             prefix : "!",
